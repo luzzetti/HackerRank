286528429d40b19ea7d211248d07a3292a3e711c
Christian <christian.luzzetti@gmail.com>
2019-12-01 20:33:22 +0100

    First Exercise

---
+++ HackerRank/src/main/java/language_proficiency/java/introduction/Exercise_001.java
@@ -0,0 +1,18 @@
+package language_proficiency.java.introduction;
+
+/*
+ * Welcome to Java!
+ * https://www.hackerrank.com/challenges/welcome-to-java/problem
+ * 
+ */
+
+public class Exercise_001 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+
+	}
+
+}
